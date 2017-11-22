b667f0bfa51150fcb23d4561fc63d5e5e3b10788
swanderz <swanson.anders@gmail.com>
2017-11-22 15:06:21 -0700

    finished?

--- codechef/TRISQ_sub.py
+++ codechef/TRISQ_sub.py
@@ -1,8 +1,5 @@
-# from sys import stdin
-# lines = [line.rstrip('\n') for line in stdin]
-
-chef = 'codechef/TRISQ_test.txt'
-lines = [int(line.rstrip('\n')) for line in open(chef)]
+from sys import stdin
+lines = [line.rstrip('\n') for line in stdin]
 
 print(lines)
 
@@ -10,11 +7,10 @@ T = lines[0]
 C = lines[1:]
 print(T,C)
 
-# from math import fmod, floor
 AnsList = []
 
 for i in range(T):
-    if C[i] <=3:
+    if C[i] <= 3:
         fx = 0
     else:
         gx = int(0.5*(C[i]-2))
