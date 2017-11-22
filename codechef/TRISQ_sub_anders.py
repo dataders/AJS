b9e236b38c9fb26485a01e797c86b6f902cfe4ff
swanderz <swanson.anders@gmail.com>
2017-11-22 15:34:04 -0700

    removed extra loop

--- codechef/TRISQ_sub_anders.py
+++ codechef/TRISQ_sub_anders.py
@@ -4,15 +4,10 @@ lines = [int(line.rstrip('\n')) for line in stdin]
 T = lines[0]
 C = lines[1:]
 
-AnsList = []
-
 for i in range(T):
     if C[i] <= 3:
         fx = 0
     else:
         gx = int(0.5*(C[i]-2))
         fx = int(0.5*gx*(gx+1))
-    AnsList.append(fx)
-
-for i in AnsList:
-    print(i)
+    print(fx)
