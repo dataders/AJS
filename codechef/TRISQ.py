8749f37a8a2c2207242518d2cc3037abe2714d02
swanderz <swanson.anders@gmail.com>
2017-11-22 14:00:16 -0700

    Merge branch 'master' of https://github.com/swanderz/AJS

--- codechef/TRISQ.py
+++ codechef/TRISQ.py
@@ -1,6 +1,7 @@
 # from sys import stdin
 # lines = [line.rstrip('\n') for line in stdin]
 
+
 chef = 'codechef/TRISQ_test.txt'
 lines = [int(line.rstrip('\n')) for line in open(chef)]
 
@@ -9,3 +10,13 @@ print(lines)
 T = lines[0]
 C = lines[1:]
 print(T,C)
+
+from math import fmod
+t = int(input())
+AnsList = []
+for i in range(t):
+    b = int(input())
+    ans = fmod((b^2/2),4)
+    AnsList.append(ans)
+for i in AnsList:
+    print(i)
