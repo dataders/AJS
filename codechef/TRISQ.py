5b3bf97bbb3da8124557cc5d7ffacac5c69a00aa
jayswans <joelswanson22@gmail.com>
2017-11-22 13:58:54 -0700

    math of it

--- codechef/TRISQ.py
+++ codechef/TRISQ.py
@@ -6,3 +6,13 @@ print(lines)
 T = lines[0]
 C = lines[1:]
 print(T,C)
+
+from math import fmod
+t = int(input())
+AnsList = []
+for i in range(t):
+    b = int(input())
+    ans = fmod((b^2/2),4)
+    AnsList.append(ans)
+for i in AnsList:
+    print(i)
