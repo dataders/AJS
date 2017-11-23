467214cf52e33641ae915a491c3ece76b4dd6634
swanderz <swanson.anders@gmail.com>
2017-11-22 18:59:02 -0700

    dot product

--- hackerearth/cost_of_array/anders.py
+++ hackerearth/cost_of_array/anders.py
@@ -15,3 +15,12 @@ for i in range(len(the_rest)):
     cost[i] = [int(x) for x in the_rest[i].split()]
 
 print(cost[0][0], cost, sep='\n')
+
+dot_og = sum([A[i]*B[i] for i in range(N)])
+
+A_new = A
+A_new[0], A_new[2] = A_new[2], A_new[0]
+dot_new = sum([A_new[i]*B[i] for i in range(N)])
+
+
+print(dot_og, dot_new)
